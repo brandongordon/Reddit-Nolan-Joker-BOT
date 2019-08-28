fb00d8e2199b90e1ee323adbd38fba84f4e8361a
brandon <brandon.j.gordon99@gmail.com>
2019-08-28 16:10:41 +0800

    Addressed bot replying to multiple comments

--- joker.py
+++ joker.py
@@ -10,16 +10,40 @@ with open("quotes.txt", "r") as f:
 	joker_quotes = f.read()
 	joker_quotes = joker_quotes.split("\n")
 	joker_quotes = list(filter(None, joker_quotes))
+	
+	
+#Have we run this code before? If not, create an empty list
+if not os.path.isfile("posts_replied_to.txt"):
+	posts_replied_to = []
+	
+#If we have run the code before, load the list of posts we have replied to
+else:
+	#Read the file into a list and remove any empty values
+	with open("posts_replied_to.txt", "r") as f:
+		posts_replied_to = f.read()
+		posts_replied_to = posts_replied_to.split("\n")
+		posts_replied_to = list(filter(None, posts_replied_to))
 
 
+
+#Create the Reddit instance
 reddit = praw.Reddit('nolan-joker-bot')
 
 subreddit = reddit.subreddit("nolanbatmanmemes")
 
 for comment in subreddit.stream.comments():
-	if re.search("joker", comment.body, re.IGNORECASE):
+	if re.search("joker", comment.body, re.IGNORECASE) and comment.id not in posts_replied_to::
 		print ("Target comment: ", comment.body)
 		joker_reply = random.choice(joker_quotes)
 		comment.reply(joker_reply)
 		print ("Joker reply: ", joker_reply)
 		print ("-----------------\n")
+		
+		#store the current ID into our list
+		posts_replied_to.append(submission.id)
+			
+		#write our updated list back to the file
+		with open("posts_replied_to.txt", "w") as f:
+			for post_id in posts_replied_to:
+				f.write(post_id + "\n")
+		
